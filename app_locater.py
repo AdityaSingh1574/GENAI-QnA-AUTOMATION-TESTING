678f9a55f95c7d004854961b5dcf6cd9e6faede6
AdityaSingh1574 <aditya.singh1@geminisolutions.com>
2024-09-16 11:31:11 +0530

    minor updates

--- app_locater.py
+++ app_locater.py
@@ -13,7 +13,15 @@ interpreter.llm.temperature = 0.1
 
 
 interpreter.chat("""
+Feature: Test Scrolling down and then up on Amazon at https://www.amazon.in
+As a user, I want to be able to scroll to the bottom of the Amazon homepage and back to the top
 
+Scenario: Scroll Down to the end of the Page and back to the top
+Given the user is on the Amazon homepage "https://www.amazon.in/"
+When the user scrolls down to the end of the page
+Then the user should see the footer content
+When the user scrolls back to the top of the page
+Then the user should see the Amazon logo and search bar
 
 For the above mentioned text of Feature file generate a locator file in python.
 locator file can be defined as follows : Python defines the locators (like XPath, CSS selectors, etc.) for various elements on the web page that will be interacted with during testing.
@@ -23,7 +31,8 @@ Follow the below instructions for completing the task
 2. Determine all the web elements that will be interacted with based on the steps in the feature file.
 3. Use selenium to navigate to visit the URL and identify the elements.Use the Selenium WebDriver to find and test the locators.
 4. Take a screenshot of every page visited during the locator identification process.Use these screenshots to verify the correctness of the locators.
-5. Save the locators as JSON in the current directory with the name of ``
+5. Save the locator file in JSON format in the current directory with the name `<website name>_<task>.json` 
+6. Also generate `test_locators.py` in the current directory which can test the locators 
                  
 Use the following steps in order to verify if the locator file is properly implemented or not
 1. Ensure that all locators uniquely identify their respective elements.
